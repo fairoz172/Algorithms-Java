98621917b4db0495c95ee239ce9f12fccb0c712c
fairoz172 <fairoz172@gmail.com>
2017-11-05 13:22:13 +0530

    Removed the SOP lines

--- src/com/fairoz/sorting/MergeSortExample.java
+++ src/com/fairoz/sorting/MergeSortExample.java
@@ -16,6 +16,7 @@ public class MergeSortExample {
         inputArray.add(2);
         inputArray.add(5);
         List<Integer> sortedArray = mergeSort.sort(inputArray);
+        System.out.println("Sorted Array : " + sortedArray);
     }
 
 }
@@ -23,7 +24,6 @@ public class MergeSortExample {
 class MergeSort {
 
     public List<Integer> sort(List<Integer> inputArray) {
-        System.out.println("Input Array = " + inputArray);
         List<Integer> mergedArray = new ArrayList<>();
         List<Integer> leftArray = null;
         List<Integer> rightArray = null;
@@ -37,30 +37,22 @@ class MergeSort {
             }
             return inputArray;
         } else if (inputArray.size() % 2 == 0) {
-            //System.out.println("Left Index : [" + 0 + ", " + inputArray.size() / 2 + "]");
-            //System.out.println("Right Index : [" + inputArray.size() / 2 + ", " + inputArray.size() + "]");
             leftArray = inputArray.subList(0, inputArray.size() / 2);
             rightArray = inputArray.subList(inputArray.size() / 2, inputArray.size());
         } else {
-            //System.out.println("Left Index : [" + 0 + ", " + (inputArray.size() + 1) / 2 + "]");
-            //System.out.println("Right Index : [" + (((inputArray.size() + 1) / 2)) + ", " + inputArray.size() + "]");
             leftArray = inputArray.subList(0, (inputArray.size() + 1) / 2);
             rightArray = inputArray.subList(((inputArray.size() + 1) / 2), inputArray.size());
 
         }
-        System.out.println("Left Array = " + leftArray);
-        System.out.println("Right Array = " + rightArray);
         List<Integer> leftSortedArray = sort(leftArray);
         List<Integer> rightSortedArray = sort(rightArray);
-        System.out.println("Left Sorted Array = " + leftSortedArray);
-        System.out.println("Right Sorted Array = " + rightSortedArray);
         int i = 0;
         int j = 0;
         for (int k = 0; k < inputArray.size(); k++) {
-            if(i == leftSortedArray.size()){
+            if (i == leftSortedArray.size()) {
                 mergedArray.addAll(rightSortedArray.subList(j, rightSortedArray.size()));
                 break;
-            }else if(j == rightSortedArray.size()){
+            } else if (j == rightSortedArray.size()) {
                 mergedArray.addAll(leftSortedArray.subList(i, leftSortedArray.size()));
                 break;
             }
@@ -72,7 +64,6 @@ class MergeSort {
                 j++;
             }
         }
-        System.out.println("Merged Array = " + mergedArray);
         return mergedArray;
     }
 }
